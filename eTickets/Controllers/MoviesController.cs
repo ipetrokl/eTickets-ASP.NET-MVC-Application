a8c14696f0bfa7735559d3d7d1127ec66c9763c7
Ivan Petrović <ipetrokl@gmail.com>
2022-01-06 13:23:49 +0100

    Edit

--- eTickets/Controllers/MoviesController.cs
+++ eTickets/Controllers/MoviesController.cs
@@ -36,8 +36,9 @@ namespace eTickets.Controllers
 
             if(!string.IsNullOrEmpty(searchString))
             {
-                var filteredResult = allMovies.Where(n => n.Name.Contains(searchString) || n.Description.Contains(searchString)).ToList();
-                return View("Index", filteredResult);
+
+                var filteredResultNew = allMovies.Where(n => string.Equals(n.Name, searchString, StringComparison.CurrentCultureIgnoreCase) || string.Equals(n.Description, searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
+                return View("Index", filteredResultNew);
             }
 
             return View("Index", allMovies);
